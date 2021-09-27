633ff25ca9a4900e06e75f57f4634839925f0f85
Kashish Jain <kjcoder04@gmail.com>
2021-09-27 18:10:54 +0530

    cors

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+  cors: { origin: process.env.CLIENT_URL, methods: ["GET", "POST"] },
 });
 
 // initialize Router
