ac565c7fc36646d26ba332f094f14836673cca1c
Kashish Jain <kjcoder04@gmail.com>
2021-10-03 18:59:09 +0530

    socket cors and user creation

--- api.js
+++ api.js
@@ -1,133 +1,141 @@
-const express = require("express");
+const express = require('express');
 // const pusher = require("./pusher.js");
-const db = require("./firebase.js").firestore;
-const functions = require("./functions");
-const http = require("http");
-const { Server } = require("socket.io");
+const db = require('./firebase.js').firestore;
+const functions = require('./functions');
+const http = require('http');
+const { Server } = require('socket.io');
 
 // initialize app
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: process.env.CLIENT_URL, methods: ["GET", "POST"] },
+	cors: { origin: '*', methods: ['GET', 'POST'] },
 });
 
 // initialize Router
 const router = express.Router();
 
 // listen to socket events
-io.on("connection", (socket) => {
-  socket.on("disconnect", () => {
-    // console.log("user disconnected");
-  });
+io.on('connection', (socket) => {
+	socket.on('disconnect', () => {
+		// console.log("user disconnected");
+	});
 
-  socket.on("userDoc", async ({ user }, callback) => {
-    if (!user) {
-      return callback("Invalid user");
-    }
-    functions.userDoc(socket, user);
-  });
+	socket.on('userDoc', async ({ user }, callback) => {
+		if (!user) {
+			return callback('Invalid user');
+		}
+		functions.userDoc(socket, user);
+	});
 
-  socket.on("customersCol", async ({ user }, callback) => {
-    if (!user) {
-      return callback("Invalid user");
-    }
-    functions.customersCol(socket, user);
-  });
-  
-  socket.on("custDoc", async ({ user, custId }, callback) => {
-    if (!user || !custId) {
-      return callback("Invalid user");
-    }
-    functions.custDoc(socket, user, custId);
-  });
+	socket.on('customersCol', async ({ user }, callback) => {
+		if (!user) {
+			return callback('Invalid user');
+		}
+		functions.customersCol(socket, user);
+	});
 
-  socket.on("transactionsCol", async ({ user, custId }, callback) => {
-    if (!user || !custId) {
-      return callback("Invalid user");
-    }
-    functions.transactionsCol(socket, user, custId);
-  });
+	socket.on('custDoc', async ({ user, custId }, callback) => {
+		if (!user || !custId) {
+			return callback('Invalid user');
+		}
+		functions.custDoc(socket, user, custId);
+	});
+
+	socket.on('transactionsCol', async ({ user, custId }, callback) => {
+		if (!user || !custId) {
+			return callback('Invalid user');
+		}
+		functions.transactionsCol(socket, user, custId);
+	});
 });
 
-router.post("/addCustomer", (req, res) => {
-  const { id, name, user } = req.body;
-  if (!id || !name || !user) {
-    return res.status(400).send("Invalid data");
-  }
-  if (id === user.email) {
-    return res.status(400).send("Invalid data");
-  }
-  db()
-    .collection("users")
-    .doc(user?.email)
-    .collection("customers")
-    .doc(id)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        return res.status(400).send("Customer already exists");
-      } else {
-        functions.addCustomer(res, user, id, name);
-      }
-    });
+router.post('/createUser', (req, res) => {
+	const { user } = req.body;
+	if (!user) {
+		return res.status(400).send('Invalid user');
+	}
+	functions.createUser(user, res);
 });
 
-router.post("/addTransaction", (req, res) => {
-  const {
-    user,
-    customerId,
-    isGiving,
-    amount,
-    desc,
-    url = "",
-    fileType,
-  } = req.body;
-  if (!user || !customerId || isGiving === null || !amount) {
-    return res.status(400).send("Invalid data");
-  }
-  const custRef = db()
-      .collection("users")
-      .doc(user?.email)
-      .collection("customers")
-      .doc(customerId),
-    transRef = custRef.collection("transactions").doc(),
-    selfRef = db()
-      .collection("users")
-      .doc(customerId)
-      .collection("customers")
-      .doc(user.email),
-    selfTransRef = selfRef.collection("transactions").doc(transRef.id);
+router.post('/addCustomer', (req, res) => {
+	const { id, name, user } = req.body;
+	if (!id || !name || !user) {
+		return res.status(400).send('Invalid data');
+	}
+	if (id === user.email) {
+		return res.status(400).send('Invalid data');
+	}
+	db()
+		.collection('users')
+		.doc(user?.email)
+		.collection('customers')
+		.doc(id)
+		.get()
+		.then((doc) => {
+			if (doc.exists) {
+				return res.status(500).send({ error: 'Customer already exists' });
+			} else {
+				functions.addCustomer(res, user, id, name);
+			}
+		});
+});
 
-  if (url === "") {
-    functions.dbUpdates(
-      res,
-      custRef,
-      transRef,
-      selfRef,
-      selfTransRef,
-      user,
-      customerId,
-      isGiving,
-      amount,
-      desc,
-      url
-    );
-  } else
-    functions.uploadFile(
-      res,
-      custRef,
-      transRef,
-      selfRef,
-      selfTransRef,
-      user,
-      customerId,
-      isGiving,
-      amount,
-      desc,
-      fileType,
-      url
-    );
+router.post('/addTransaction', (req, res) => {
+	const {
+		user,
+		customerId,
+		isGiving,
+		amount,
+		desc,
+		url = '',
+		fileType,
+	} = req.body;
+	if (!user || !customerId || isGiving === null || !amount) {
+		return res.status(400).send('Invalid data');
+	}
+	const custRef = db()
+			.collection('users')
+			.doc(user?.email)
+			.collection('customers')
+			.doc(customerId),
+		transRef = custRef.collection('transactions').doc(),
+		selfRef = db()
+			.collection('users')
+			.doc(customerId)
+			.collection('customers')
+			.doc(user.email),
+		selfTransRef = selfRef.collection('transactions').doc(transRef.id);
+
+	if (url === '') {
+		functions.dbUpdates(
+			res,
+			custRef,
+			transRef,
+			selfRef,
+			selfTransRef,
+			user,
+			customerId,
+			isGiving,
+			amount,
+			desc,
+			url
+		);
+	} else
+		functions.uploadFile(
+			res,
+			custRef,
+			transRef,
+			selfRef,
+			selfTransRef,
+			user,
+			customerId,
+			isGiving,
+			amount,
+			desc,
+			fileType,
+			url
+		);
 });
 
 module.exports = { app, router, server };
--- functions.js
+++ functions.js
@@ -1,259 +1,277 @@
-const db = require("./firebase.js").firestore;
-const storage = require("./firebase.js").storage;
-const stream = require("stream");
+const db = require('./firebase.js').firestore;
+const storage = require('./firebase.js').storage;
+const stream = require('stream');
 
 const uploadFile = (
-  res,
-  custRef,
-  transRef,
-  selfRef,
-  selfTransRef,
-  user,
-  customerId,
-  isGiving,
-  amount,
-  desc,
-  fileType,
-  base64
+	res,
+	custRef,
+	transRef,
+	selfRef,
+	selfTransRef,
+	user,
+	customerId,
+	isGiving,
+	amount,
+	desc,
+	fileType,
+	base64
 ) => {
-  const bufferStream = new stream.PassThrough();
-  bufferStream.end(new Buffer.from(base64.split("base64,")[1], "base64"));
-  const storageRef = storage().bucket("bahi-book.appspot.com");
-  const file = storageRef.file(`receipts/${transRef.id}`);
-  bufferStream
-    .pipe(file.createWriteStream({ metadata: { contentType: fileType } }))
-    .on("error", () => {
-      res.status(500).send("Error Uploading File");
-    })
-    .on("finish", (fileRet) => {
-      // The file upload is complete.
-      console.log(fileRet);
-      file?.getSignedUrl(
-        { action: "read", expires: "01-01-2500" },
-        (err, signedUrl) => {
-          if (err) {
-            res.status(500).send("Error Uploading File");
-          } else {
-            dbUpdates(
-              res,
-              custRef,
-              transRef,
-              selfRef,
-              selfTransRef,
-              user,
-              customerId,
-              isGiving,
-              amount,
-              desc,
-              signedUrl
-            );
-          }
-        }
-      );
-    });
+	const bufferStream = new stream.PassThrough();
+	bufferStream.end(new Buffer.from(base64.split('base64,')[1], 'base64'));
+	const storageRef = storage().bucket('bahi-book.appspot.com');
+	const file = storageRef.file(`receipts/${transRef.id}`);
+	bufferStream
+		.pipe(file.createWriteStream({ metadata: { contentType: fileType || 'image/*' } }))
+		.on('error', () => {
+			res.status(500).send('Error Uploading File');
+		})
+		.on('finish', (fileRet) => {
+			// The file upload is complete.
+			console.log(fileRet);
+			file?.getSignedUrl(
+				{ action: 'read', expires: '01-01-2500' },
+				(err, signedUrl) => {
+					if (err) {
+						res.status(500).send('Error Uploading File');
+					} else {
+						dbUpdates(
+							res,
+							custRef,
+							transRef,
+							selfRef,
+							selfTransRef,
+							user,
+							customerId,
+							isGiving,
+							amount,
+							desc,
+							signedUrl
+						);
+					}
+				}
+			);
+		});
 };
 
 const dbUpdates = (
-  res,
-  custRef,
-  transRef,
-  selfRef,
-  selfTransRef,
-  user,
-  customerId,
-  isGiving,
-  amount,
-  desc,
-  url = ""
+	res,
+	custRef,
+	transRef,
+	selfRef,
+	selfTransRef,
+	user,
+	customerId,
+	isGiving,
+	amount,
+	desc,
+	url = ''
 ) => {
-  let amt = 0;
-  if (isGiving) {
-    amt = -1 * Number(amount);
-  } else {
-    amt = Number(amount);
-  }
-  const lastActivity = db.FieldValue.serverTimestamp();
+	let amt = 0;
+	if (isGiving) {
+		amt = -1 * Number(amount);
+	} else {
+		amt = Number(amount);
+	}
+	const lastActivity = db.FieldValue.serverTimestamp();
 
-  // Adding transaction in customer's transactions collection
-  transRef.set({
-    timestamp: lastActivity,
-    amount: amt,
-    desc,
-    receipt: url,
-    by: user.email,
-  });
+	// Adding transaction in customer's transactions collection
+	transRef.set({
+		timestamp: lastActivity,
+		amount: amt,
+		desc,
+		receipt: url,
+		by: user.email,
+	});
 
-  // Adding transaction in self transactions collection
-  selfTransRef.set({
-    timestamp: lastActivity,
-    amount: -1 * amt,
-    desc,
-    receipt: url,
-    by: user.email,
-  });
-  if (isGiving) {
-    // Updating self sent amount
-    db()
-      .collection("users")
-      .doc(user?.email)
-      .update({ sent: db.FieldValue.increment(Math.abs(amt)) });
-    // Updating customer's received amount
-    db()
-      .collection("users")
-      .doc(customerId)
-      .update({ received: db.FieldValue.increment(Math.abs(amt)) });
-  } else {
-    // Updating self received amount
-    db()
-      .collection("users")
-      .doc(user.email)
-      .update({ received: db.FieldValue.increment(Math.abs(amt)) });
-    // Updating customer's sent amount
-    db()
-      .collection("users")
-      .doc(customerId)
-      .update({ sent: db.FieldValue.increment(Math.abs(amt)) });
-  }
-  // Updating customer's balance and activity
-  custRef.update({
-    balance: db.FieldValue.increment(amt),
-    lastActivity,
-  });
-  // Updating self balance and activity
-  selfRef
-    .update({
-      balance: db.FieldValue.increment(-1 * amt),
-      lastActivity,
-    })
-    .then(() => {
-      res.status(200).send("Request Successful");
-    })
-    .catch((err) => console.log(err));
+	// Adding transaction in self transactions collection
+	selfTransRef.set({
+		timestamp: lastActivity,
+		amount: -1 * amt,
+		desc,
+		receipt: url,
+		by: user.email,
+	});
+	if (isGiving) {
+		// Updating self sent amount
+		db()
+			.collection('users')
+			.doc(user?.email)
+			.update({ sent: db.FieldValue.increment(Math.abs(amt)) });
+		// Updating customer's received amount
+		db()
+			.collection('users')
+			.doc(customerId)
+			.update({ received: db.FieldValue.increment(Math.abs(amt)) });
+	} else {
+		// Updating self received amount
+		db()
+			.collection('users')
+			.doc(user.email)
+			.update({ received: db.FieldValue.increment(Math.abs(amt)) });
+		// Updating customer's sent amount
+		db()
+			.collection('users')
+			.doc(customerId)
+			.update({ sent: db.FieldValue.increment(Math.abs(amt)) });
+	}
+	// Updating customer's balance and activity
+	custRef.update({
+		balance: db.FieldValue.increment(amt),
+		lastActivity,
+	});
+	// Updating self balance and activity
+	selfRef
+		.update({
+			balance: db.FieldValue.increment(-1 * amt),
+			lastActivity,
+		})
+		.then(() => {
+			res.status(200).send({message: 'Request Successful'});
+		})
+		.catch((err) => console.log(err));
 };
 
 const addCustomer = (res, user, id, name) => {
-  const lastActivity = db.FieldValue.serverTimestamp();
+	const lastActivity = db.FieldValue.serverTimestamp();
 
-  // Add customer to user's customers collection
-  db()
-    .collection("users")
-    .doc(user?.email)
-    .collection("customers")
-    .doc(id)
-    .set({ name, balance: 0, lastActivity });
+	// Add customer to user's customers collection
+	db()
+		.collection('users')
+		.doc(user?.email)
+		.collection('customers')
+		.doc(id)
+		.set({ name, balance: 0, lastActivity });
 
-  db()
-    .collection("users")
-    .doc(user?.email)
-    .update({
-      customers: { [id]: name },
-    });
+	db()
+		.collection('users')
+		.doc(user?.email)
+		.update({
+			customers: { [id]: name },
+		});
 
-  // Create customer's doc if not exist
-  db()
-    .collection("users")
-    .doc(id)
-    .set({ customers: { [user.email]: user.name } }, { merge: true });
+	// Create customer's doc if not exist
+	db()
+		.collection('users')
+		.doc(id)
+		.set({ customers: { [user.email]: user.name } }, { merge: true });
 
-  // Add user to customer's customers collection
-  db()
-    .collection("users")
-    .doc(id)
-    .collection("customers")
-    .doc(user?.email)
-    .set({ name: user?.name, balance: 0, lastActivity })
-    .then(() => res.status(200).send("Request Successful"))
-    .catch((err) => console.log(err.message));
+	// Add user to customer's customers collection
+	db()
+		.collection('users')
+		.doc(id)
+		.collection('customers')
+		.doc(user?.email)
+		.set({ name: user?.name, balance: 0, lastActivity })
+		.then(() => res.status(200).send({message: 'Request Successful'}))
+		.catch(() => res.status(500).send({error: 'Invalid Error'}));
+};
+
+const createUser = (user, res) => {
+	db()
+		.collection('users')
+		.doc(user.email)
+		.set({ name: user.name }, { merge: true })
+    .then(() => res.status(200).send('User registered successfully'))
+		.catch(() => res.status(500).send('An error occurred'));
 };
 
 // realtime firebase functions
 const userDoc = (socket, user) => {
-  db()
-    .collection("users")
-    .doc(user.email)
-    .onSnapshot((snap) => {
-      if (snap.exists) {
-        socket.emit("userDoc", { data: snap.data() });
-      } else {
-        socket.emit("userDoc", { data: false });
-      }
-    });
+	db()
+		.collection('users')
+		.doc(user.email)
+		.onSnapshot((snap) => {
+			if (snap.exists) {
+				socket.emit('userDoc', { data: snap.data() });
+			} else {
+				socket.emit('userDoc', { data: false });
+			}
+		});
 };
 
 const customersCol = (socket, user) => {
-  db()
-    .collection("users")
-    .doc(user?.email)
-    .collection("customers")
-    .orderBy("lastActivity", "desc")
-    .onSnapshot((snap) => {
-      let cst = [];
-      snap.forEach((doc) => {
-        cst.push({
-          id: doc?.id,
-          name: doc?.data()?.name,
-          balance: doc?.data()?.balance,
-        });
-      });
-      socket.emit("customersCol", { data: cst });
-    });
+	db()
+		.collection('users')
+		.doc(user?.email)
+		.collection('customers')
+		.orderBy('lastActivity', 'desc')
+		.onSnapshot((snap) => {
+			let cst = [];
+			snap.forEach((doc) => {
+				cst.push({
+					id: doc?.id,
+					name: doc?.data()?.name,
+					balance: doc?.data()?.balance,
+				});
+			});
+			socket.emit('customersCol', { data: cst });
+		});
 };
 
 const custDoc = (socket, user, custId) => {
-  db()
-    .collection("users")
-    .doc(user?.email)
-    .collection("customers")
-    .doc(custId)
-    .onSnapshot((snap) => {
-      if (snap.exists) {
-        socket.emit("custDoc", { data: snap.data() });
-      }
-    });
+	db()
+		.collection('users')
+		.doc(user?.email)
+		.collection('customers')
+		.doc(custId)
+		.onSnapshot((snap) => {
+			if (snap.exists) {
+				socket.emit('custDoc', { data: snap.data() });
+			}
+		});
 };
 
 const transactionsCol = (socket, user, custId) => {
-  db()
-    .collection("users")
-    .doc(user?.email)
-    .collection("customers")
-    .doc(custId)
-    .onSnapshot((snapshot) => {
-      snapshot.ref
-        .collection("transactions")
-        .orderBy("timestamp", "desc")
-        .onSnapshot(async (snap) => {
-          const customerName = await snapshot.ref
-            .get()
-            .then((doc) => doc.data().name);
-          let transactions = [],
-            sent = 0,
-            received = 0;
-          snap.docs.forEach((doc) => {
-            transactions.push({
-              id: doc.id,
-              amount: doc.data().amount,
-              receipt: doc.data().receipt,
-              timestamp: doc.data().timestamp,
-              desc: doc.data().desc,
-              by:
-                doc.data().by === ""
-                  ? "NA"
-                  : doc.data().by === user?.email
-                  ? user?.name
-                  : customerName,
-            });
-            doc.data().amount >= 0
-              ? (received += Number(doc.data().amount))
-              : (sent -= Number(doc.data().amount));
-          });
-          socket.emit("transactionsCol", {
-            data: { transactions, sent, received },
-          });
-        });
-    });
+	db()
+		.collection('users')
+		.doc(user?.email)
+		.collection('customers')
+		.doc(custId)
+		.onSnapshot((snapshot) => {
+			snapshot.ref
+				.collection('transactions')
+				.orderBy('timestamp', 'desc')
+				.onSnapshot(async (snap) => {
+					const customerName = await snapshot.ref
+						.get()
+						.then((doc) => doc.data().name);
+					let transactions = [],
+						sent = 0,
+						received = 0;
+					snap.docs.forEach((doc) => {
+						transactions.push({
+							id: doc.id,
+							amount: doc.data().amount,
+							receipt: doc.data().receipt,
+							timestamp: doc.data().timestamp,
+							desc: doc.data().desc,
+							by:
+								doc.data().by === ''
+									? 'NA'
+									: doc.data().by === user?.email
+									? user?.name
+									: customerName,
+						});
+						doc.data().amount >= 0
+							? (received += Number(doc.data().amount))
+							: (sent -= Number(doc.data().amount));
+					});
+					socket.emit('transactionsCol', {
+						data: { transactions, sent, received },
+					});
+				});
+		});
 };
 
-const functions = { dbUpdates, uploadFile, addCustomer, userDoc, customersCol, custDoc, transactionsCol };
+const functions = {
+	dbUpdates,
+	uploadFile,
+	addCustomer,
+	userDoc,
+	customersCol,
+	custDoc,
+	transactionsCol,
+  createUser,
+};
 
 module.exports = functions;
